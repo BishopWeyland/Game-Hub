a1114c9f65c80cb7ecaab21876a83beb5e436bb4
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-10-01 16:37:06 +0200

    shopping cart

---
+++ js/shopping-cart.js
@@ -0,0 +1,13 @@
+const addCart = document.querySelectorAll("button.add-cart");
+const cart = document.querySelector(".cart-container");
+const exitCart = document.querySelector(".exit");
+
+for (let i = 0; i < addCart.length; i++) {
+  addCart[i].addEventListener("click", () => {
+    cart.classList.toggle("show");
+  });
+}
+
+exitCart.addEventListener("click", () => {
+  cart.classList.remove("show");
+});
