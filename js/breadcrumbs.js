733bc3fd41be868e3fa13166f8932a087491a535
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-10-27 13:36:58 +0200

    breadcrumbs java

--- js/breadcrumbs.js
+++ js/breadcrumbs.js
@@ -1,3 +1,5 @@
+import { queryString, params, id, url } from "./game-title.js";
+
 const backButton = document.querySelector("#backButton");
 
 backButton.addEventListener("click", () => {
@@ -9,3 +11,13 @@ if (document.referrer.includes("search.html")) {
 } else {
   backButton.style.display = "none";
 }
+
+const breadcrumb = document.querySelector(".breadcrumb-title");
+
+async function getBreadcrumbs() {
+  const response = await fetch(url);
+  const results = await response.json();
+  breadcrumb.innerHTML = `<p>${results.name}</p>`;
+}
+
+getBreadcrumbs();
--- js/game-title.js
+++ js/game-title.js
@@ -1,6 +1,4 @@
-const image = document.querySelector(".game-photo");
-const productInfo = document.querySelector(".flexbox-item-2");
-const price = document.querySelector(".price-flex");
+export { queryString, params, id, url };
 
 const queryString = document.location.search;
 
@@ -9,6 +7,10 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 const url = `https://willand.tech/gh_api/wp-json/wc/v3/products/${id}?consumer_key=ck_50fb01302cab00f3eb7a2adfaa8b64fc78d2188f&consumer_secret=cs_b2e4bb1c758637e4cf18f88760dd7acb8b2ba2da`;
+
+const image = document.querySelector(".game-photo");
+const productInfo = document.querySelector(".flexbox-item-2");
+const price = document.querySelector(".price-flex");
 
 async function getDetails() {
   try {
