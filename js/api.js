ba837bbd9fd168745259c76c4939b3b122d95d0d
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-10-27 12:48:45 +0200

    loads of stuff

---
+++ js/api.js
@@ -0,0 +1,26 @@
+const url =
+  "https://willand.tech/gh_api/wp-json/wc/v3/products?consumer_key=ck_50fb01302cab00f3eb7a2adfaa8b64fc78d2188f&consumer_secret=cs_b2e4bb1c758637e4cf18f88760dd7acb8b2ba2da";
+const productGrid = document.querySelectorAll("#grid");
+
+async function getProducts() {
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+    console.log(results);
+    for (let i = 0; i < productGrid.length; i++) {
+      results.forEach(function (results) {
+        productGrid[
+          i
+        ].innerHTML += `<div class="product-card"><a href="game-title.html?id=${results.id}"><img src="${results.images[0].src}"> <h3>${results.name}</h3>
+      <div class="inner-grid">
+      <p class="price">${results.price}$</p></a>
+      <i class="fa-regular fa-heart favorites"></i>
+     
+      </div> <button class=" btn btnaddcart add-cart">
+              Add to cart</button></div>`;
+      });
+    }
+  } catch (error) {}
+}
+
+getProducts(url);
---
+++ js/game-title.js
@@ -0,0 +1,30 @@
+const image = document.querySelector(".game-photo");
+const productInfo = document.querySelector(".flexbox-item-2");
+const price = document.querySelector(".price-flex");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+const url = `https://willand.tech/gh_api/wp-json/wc/v3/products/${id}?consumer_key=ck_50fb01302cab00f3eb7a2adfaa8b64fc78d2188f&consumer_secret=cs_b2e4bb1c758637e4cf18f88760dd7acb8b2ba2da`;
+
+async function getDetails() {
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+    console.log(results);
+    image.innerHTML = `<img src="${results.images[0].src}">`;
+
+    productInfo.innerHTML = `
+    <h1>${results.name}</h1>
+    <h2>${results.tags[0].name}</h2>
+    <p>${results.description}</p>`;
+
+    price.innerHTML = `<h3>${results.price}$</h3>
+    <i class="fa-regular fa-heart favorites"></i>`;
+  } catch (error) {}
+}
+
+getDetails();
