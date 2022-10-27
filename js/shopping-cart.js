87393a38dce49be37782bf9692f65c807bd2cff0
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-10-27 14:40:20 +0200

    cart update

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -13,3 +13,24 @@ for (let i = 0; i < addCart.length; i++) {
 exitCart.addEventListener("click", () => {
   cart.classList.remove("show");
 });
+
+const cartPhoto = document.querySelector(".cart-photo");
+const price = document.querySelector(".item-price");
+const total = document.querySelector(".total-price");
+
+async function shoppingCart() {
+  const response = await fetch(url);
+  const results = await response.json();
+
+  cartPhoto.innerHTML = `<img src="${results.images[0].src}">`;
+  price.innerHTML = `
+  <p> 1 x ${results.name}</p>
+  <p>${results.price}$</p>`;
+
+  const totalPrice = parseFloat(results.price);
+  total.innerHTML = `
+  <p>Total:</p>
+  <p>${totalPrice + 10}$</p>`;
+}
+
+shoppingCart();
