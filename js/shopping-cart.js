526b861417e3d356d6b32a98008ed0d2eee67d69
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-10-28 12:25:00 +0200

    test

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -17,6 +17,7 @@ exitCart.addEventListener("click", () => {
 const cartPhoto = document.querySelector(".cart-photo");
 const price = document.querySelector(".item-price");
 const total = document.querySelector(".total-price");
+const checkoutButton = document.querySelector(".checkout-button");
 
 async function shoppingCart() {
   const response = await fetch(url);
